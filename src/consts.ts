af5f7fa88cf28c00f3e3a6e21f7b379fdc561dcc
EdgarPureco <pureco.edgar.1am@gmail.com>
2024-06-06 23:20:49 -0600

    First Version Done

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,39 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Triciclos Bikes León';
+export const SITE_DESCRIPTION = 'Taller Mecánico de Bicicletas';
+export const AVAILABLE_SERVICES = [
+    {
+        "title": "Ensamblaje de piezas",
+        "description": "Montaje y ajuste de todas las piezas de tu bicicleta para asegurar un funcionamiento óptimo."
+    },
+    {
+        "title": "Armado de Bicicletas",
+        "description": "Armado completo de bicicletas nuevas a partir de piezas sueltas o kit de montaje."
+    },
+    {
+        "title": "Ajuste de Cambios",
+        "description": "Ajuste preciso de los sistemas de cambio de marchas para un cambio suave y eficiente."
+    },
+    {
+        "title": "Balanceo de Aros",
+        "description": "Equilibrado de las ruedas para mejorar la estabilidad y el rendimiento en la conducción."
+    },
+    {
+        "title": "Armado de Aros",
+        "description": "Construcción y ajuste de aros para garantizar una rueda duradera y de alto rendimiento."
+    },
+    {
+        "title": "Parches",
+        "description": "Reparación de pinchazos y daños menores en las cámaras de aire de las ruedas."
+    },
+    {
+        "title": "Lavado y Engrasado",
+        "description": "Limpieza completa de la bicicleta y lubricación de todas las partes móviles para un mantenimiento adecuado."
+    },
+    {
+        "title": "Cotizaciones",
+        "description": "Estimaciones detalladas de costos para reparaciones y servicios personalizados de bicicletas."
+    }
+]
